34c215c69c6ff0d28b721e19fde69b6967544d10
jngsngjn <wjdtjdwls98@gmail.com>
2024-05-14 16:23:09 +0900

    로그인 검증

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,12 +1,15 @@
-$(document).ready(function() {
-    $("#loginForm").on("submit", function(event) {
-        const username = $("#username").val().trim();
-        const password = $("#password").val().trim();
+function validateForm() {
+    var loginId = document.getElementById('username').value.trim();
+    var password = document.getElementById('password').value.trim();
+    var errorDiv = document.getElementById('inputError');
+    var serverErrorDiv = document.getElementById('serverError');
 
-        if (username === "" || password === "") {
-            alert("아이디와 비밀번호를 입력해주세요.");
-            event.preventDefault(); // 폼 제출 중단
-            return false;
+    if (loginId === "" || password === "") {
+        errorDiv.textContent = '아이디와 비밀번호를 입력해 주세요.';
+        if (serverErrorDiv) {
+            serverErrorDiv.style.display = 'none'; // serverError 숨김
         }
-    });
-});
+        return false;
+    }
+    return true;
+}
